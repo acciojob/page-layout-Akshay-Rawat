88be530589a1b644b302f07bbf5be56828bb9eb2
Akshay-Web-ops <rawat.akshay@webseeder.tech>
2026-04-23 12:47:09 +0530

    Clean assignment upload

---
+++ src/components/Page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Page=  ()=>{
+    return(
+        <div>
+            <h1>Welcome to my we bsite</h1>
+            <p>This is the content of my website.</p>
+            <p>© 2023 My Website.All rights reserved</p>
+        </div>
+    )
+}
